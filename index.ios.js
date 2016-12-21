6b03579ea83909b460bdd1c0fbef30c253aa0b4e
Ruben Quintero <rbnquintero@gmail.com>
2016-12-21 12:24:44 -0600

    initial setup

---
+++ index.ios.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+var Parse = require('parse/react-native')
+Parse.initialize("HPry3wMRFE8fulZJQmvpbSAAPRJSgpMOpiDr5GqB","kwEYlmoTBP98IqHgDbXaH3ZOyQnTi1kKafRCxjLk")
+Parse.serverURL='https://parseapi.back4app.com/'
+
+export default class enciendepilot extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('enciendepilot', () => enciendepilot);
